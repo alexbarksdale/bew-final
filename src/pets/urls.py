7eb1dbc3912f43c0eb4679708d4fe2dfdb7572f0
alexbarksdale <ale.barksdale@gmail.com>
2020-05-11 01:44:04 -0500

    [Add] Pet list, pet details, pet create

--- src/pets/urls.py
+++ src/pets/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
-from pets.views import HomeView, PetCreateView, PetsView
+from pets.views import HomeView, PetCreateView, PetsListView, PetDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home-page'),
+    path('pets/', PetsListView.as_view(), name='pets-list-page'),
     path('create/', PetCreateView.as_view(), name='pet-create-page'),
-    path('pets/', PetsView.as_view(), name='pets-page')
+    path('pets/<int:pet_id>/', PetDetailView.as_view(), name='pet-detail-page'),
+
 ]
--- src/pets/views.py
+++ src/pets/views.py
@@ -16,15 +16,25 @@ class HomeView(ListView):
 
 class PetCreateView(CreateView):
     model = Pet
-    fields = ['pet_name', 'species', 'breed', 'weight_in_pounds', 'owner']
+    fields = ['pet_name', 'species', 'breed', 'weight_in_pounds']
     template_name = 'create_pet.html'
 
 
-class PetsView(ListView):
+class PetsListView(ListView):
+    '''Render created pets'''
     model = Pet
 
     def get(self, req):
         pets = self.get_queryset().all()
-        return render(req, 'pets.html', {
+        return render(req, 'pet/pet_list.html', {
             'pets': pets
+        })
+
+
+class PetDetailView(DetailView):
+    '''Render pet information'''
+
+    def get(self, request, pet_id):
+        return render(request, 'pet/pet_detail.html', {
+            'pet': Pet.objects.get(id=pet_id)
         })
