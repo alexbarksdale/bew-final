cb25a1ba40dc06031ea37b6dfb7b5081ba509426
alexbarksdale <ale.barksdale@gmail.com>
2020-05-11 15:40:37 -0500

    [Add] Detail page

--- src/pets/tests.py
+++ src/pets/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 from pets.models import Pet, Appointment
 
 
@@ -17,8 +19,44 @@ class PetTests(TestCase):
         pet.save()  # save our pet
 
         # Making a GET request to check if the pet is in the results (list page)
+        res = self.client.get(f'/pets/')
+
+        # Very a 200 response
+        self.assertEqual(res.status_code, 200)
+
+        # Check if the page contains a pet with the name Ricky
+        self.assertContains(res, 'Ricky')
+
+        # Get pet object to check if the user's pet exists
+        pet_object = Pet.objects.get(owner=user)
+        self.assertEqual(pet_object.owner, user)
+
+    def test_detail_page(self):
+        # Instance of user to test the pages
+        user = User.objects.create_user(username='test', password='testing123')
+        self.client.login(username='test', password='testing123')
+
+        # Create a test pet
+        pet = Pet.objects.create(pet_name="Ricky",
+                                 species="Dog",
+                                 breed='Husky',
+                                 weight_in_pounds=55.6,
+                                 owner=user)
+        pet.save()  # save our pet
+
+        # Create an appointment
+        appointment = Appointment.objects.create(date_of_appointment=timezone.now(),
+                                                 duration_minutes=30,
+                                                 special_instructions='Leave door open',
+                                                 pet=pet)
+        appointment.save()  # save appointment
+
+        # Making a GET request to check if the pet is in the results (detail page)
         res = self.client.get(f'/pets/{pet.id}/')
         # Very a 200 response
         self.assertEqual(res.status_code, 200)
-        # Check if the page contains Captain America (our title)
-        self.assertContains(res, 'Ricky')
+
+        # Get pet object to check
+        pet_object = Pet.objects.get(pet_name='Ricky')
+        # Check if the pet's name appears in the result
+        self.assertEqual(pet_object.pet_name, 'Ricky')
