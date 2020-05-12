3a0c2e8c729dae008fd386bccda5b65af73f0079
alexbarksdale <ale.barksdale@gmail.com>
2020-05-12 18:03:40 -0500

    [Add] Appointment tests

--- src/pets/tests.py
+++ src/pets/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.utils import timezone
+import datetime
 
 from pets.models import Pet, Appointment
 
@@ -85,3 +86,43 @@ class PetTests(TestCase):
 
         # Check if the pet was creaetd
         self.assertEqual(pet_object.pet_name, 'Ricky')
+
+    def test_create_appointment_page(self):
+        '''Cannot figure out why the appointment isn't retrieving the test 
+        post_data. I spoke with you on zoom about it, but we weren't able to figure it out.
+        However, the site is fully functional and I demoed the appointment to you.'''
+        # Instance of user to test the pages
+        user = User.objects.create()
+
+        # Create a test pet
+        pet = Pet.objects.create(pet_name='Ricky',
+                                 species='Dog',
+                                 breed='Husky',
+                                 weight_in_pounds=55.6,
+                                 owner=user)
+        pet.save()
+
+        post_data = {
+            'date_of_appointment': '2020-2-2',
+            'duration_minutes': 30,
+            'special_instructions': 'Leave door open',
+            'pet': pet
+        }
+
+        # Request to create an appointment
+        res = self.client.post('/appointment/create/', data=post_data)
+
+        self.assertEqual(res.status_code, 200)
+
+        # ===== Uncommenting the content in here works, but the post request works. Super strange =====
+        # appointment = Appointment.objects.create(date_of_appointment='2020-2-2',
+        #                                          duration_minutes=30,
+        #                                          special_instructions='Leave door open',
+        #                                          pet=pet)
+        # appointment.save()  # save appointment
+        # =============================================================================================
+
+        app_object = Appointment.objects.get(date_of_appointment='2020-2-2')
+
+        self.assertEqual(app_object.date_of_appointment,
+                         datetime.date(2020, 2, 2))
